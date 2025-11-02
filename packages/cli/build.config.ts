bb6c27005cd754ee90ec8e8dde68139706253a28
nethris <david.abou@epitech.eu>
2025-11-02 13:05:10 +0100

    refactor(cli): migrate to tsdown

--- packages/cli/build.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineBuildConfig } from 'unbuild'
-
-export default defineBuildConfig({
-  declaration: true,
-  entries: ['./src/index', './src/runtime/harmonix'],
-  externals: ['harmonix', 'discord.js', /^#harmonix-virtual\//]
-})
