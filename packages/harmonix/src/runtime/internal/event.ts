536b8bc6b1ed1af7b30d7fdb4127f8b3e231c93e
nethris <david.abou@epitech.eu>
2025-12-14 09:23:47 +0100

    feat: add config overload for `defineEvent`

--- packages/harmonix/src/runtime/internal/event.ts
+++ packages/harmonix/src/runtime/internal/event.ts
@@ -5,14 +5,29 @@ import type { ClientEvents } from 'discord.js'
 import type { HarmonixEventCallback } from '../../types/runtime/event'
 import type { HarmonixEvent } from '../../types/module'
 
-export const defineEvent = <T extends keyof ClientEvents>(
+export function defineEvent<T extends keyof ClientEvents>(
+  config: { name: T; once?: boolean },
+  callback: HarmonixEventCallback<T>
+): HarmonixEvent
+
+export function defineEvent<T extends keyof ClientEvents>(
   name: T,
   callback: HarmonixEventCallback<T>
-) => {
+): HarmonixEvent
+
+export function defineEvent<T extends keyof ClientEvents>(
+  nameOrConfig: T | { name: T; once?: boolean },
+  callback: HarmonixEventCallback<T>
+) {
+  const name =
+    typeof nameOrConfig === 'string' ? nameOrConfig : nameOrConfig.name
+  const once =
+    typeof nameOrConfig === 'string' ? false : (nameOrConfig.once ?? false)
+
   return {
     type: ModuleType.Event,
     name,
-    once: false,
+    once,
     callback
   } as HarmonixEvent
 }
